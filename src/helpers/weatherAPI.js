b04782695f2070892a7ec04640d76aad0f551117
Matheus Motta <matheusmotta136@gmail.com>
2023-05-25 16:29:50 -0300

    Requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,19 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  try {
+    const resposta = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+    const data = await resposta.json();
+
+    if (data.length === 0) {
+      throw new Error('Nenhuma cidade encontrada');
+    }
+    return data;
+  } catch (error) {
+    window.alert(error.message);
+  }
 };
 
-export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+export const getWeatherByCity = () => {
 };
