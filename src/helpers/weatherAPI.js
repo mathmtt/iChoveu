59fb61af8407a96e37c194e54de51c66bf8b4c6b
Matheus Motta <matheusmotta136@gmail.com>
2023-05-25 17:09:41 -0300

    Requisito 2

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,5 +1,16 @@
 // Remova os comentários a medida que for implementando as funções
 const TOKEN = import.meta.env.VITE_TOKEN;
+export const getWeatherByCity = async (cityURL) => {
+  const resposta = await fetch(`http://api.weatherapi.com/v1/current.json?lang=pt&key=${TOKEN}&q=${cityURL}`);
+  const data = await resposta.json();
+  const obj = {
+    temp: data.current.temp_c,
+    icon: data.current.condition.icon,
+    condition: data.current.condition.text,
+  };
+  console.log(obj);
+  return obj;
+};
 
 export const searchCities = async (term) => {
   try {
@@ -9,11 +20,10 @@ export const searchCities = async (term) => {
     if (data.length === 0) {
       throw new Error('Nenhuma cidade encontrada');
     }
+    data.forEach((tempCity) => getWeatherByCity(tempCity.url));
+    console.log(data);
     return data;
   } catch (error) {
     window.alert(error.message);
   }
-};
-
-export const getWeatherByCity = () => {
 };
